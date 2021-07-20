e96069690f7b400d18644194e722fc10b9fcde7b
colin <colinstanford092@gmail.com>
2021-07-20 16:03:11 -0600

    while loop

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace battleship
     {
         static void Main(string[] args)
         {
+            // run game info function
             getGameInfo();
 
             // define firm locations for the ship
@@ -18,19 +19,39 @@ namespace battleship
             int guess;
 
             // define a hits variable that will add up when user guesses correctly
-            int hits;
+            int hits = 0;
 
             // define a guesses variable that will add up every incorrect guess
-            int guesses;
+            int guesses = 0;
 
             // define a boolean variable that will be false until the ship is sunk
             bool isSunk = false;
 
+            // ask user for their name
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Welcome {0}, let's play battleship!", name);
 
-            // while (isSunk == false) {
-            //     // get user input
 
-            // }
+            while (isSunk == false) {
+                // get user input
+                Console.WriteLine("Enter a number between 1 and 6: ");
+                guess = Console.ReadLine();
+
+                if (guess != Int32.TryParse(1, 6)) {
+                    Console.ForegroundColor = ConsoleColor.Red;           
+                    Console.WriteLine("Please enter a valid number (1-6)");
+                    Console.ResetColor();
+                }
+
+                if (guess != location1 || guess != location2 || guess != location3) {
+                    Console.ForegroundColor = ConsoleColor.Red;           
+                    Console.WriteLine("MISS! Try again");
+                    guesses = guesses + 1;
+                    Console.ResetColor();
+                }
+
+            }
 
 
         }
