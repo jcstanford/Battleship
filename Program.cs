c3c3741c3ec8e509da20132901cf6ab300f88213
colin <colinstanford092@gmail.com>
2021-07-20 15:19:13 -0600

    other variables

--- Program.cs
+++ Program.cs
@@ -12,8 +12,18 @@ namespace battleship
             int location2 = 3;
             int location3 = 4;
 
+            // define a guess variable that will prompt user input
             int guess;
-            
+
+            // define a hits variable that will add up when user guesses correctly
+            int hits;
+
+            // define a guesses variable that will add up every incorrect guess
+            int guesses;
+
+            // define a boolean variable that will be false until the ship is sunk
+            bool isSunk = false;
+
 
         }
     }
